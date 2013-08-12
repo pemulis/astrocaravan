a416f67831848210b764c33319750daadf454e7c
John Shutt <john.d.shutt@gmail.com>
2013-08-12 15:58:33 -0700

    fill out events and projects controllers and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,44 @@
 class EventsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @commentable = @event
+    @comments = @commentable.comments
+    @comment = Comment.new
+  end
+
+  def create
+    @event = current_user.events.create(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Your event post was successful!'
+    else
+      notice = 'Something went wrong when we tried to add your event...'
+      render action: 'new'
+    end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :image_url, :location, :date)
+  end
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,43 @@
 class ProjectsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @commentable = @project
+    @comments = @commentable.comments
+    @comment = Comment.new
+  end
+
+  def create
+    @project = current_user.projects.create(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Your project post was successful!'
+    else
+      notice = 'Something went wrong when we tried to add your project...'
+      render action: 'new'
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :image_url, :location)
+  end
 end
